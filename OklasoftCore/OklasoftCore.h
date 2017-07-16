3af5de4b6545474b6af0ee2154f77492084492ec
Justin Oakes <justin@Justins-MacBook-Pro.local>
2017-07-16 16:02:09 -0500

    Initial Commit

---
+++ OklasoftCore/OklasoftCore.h
@@ -0,0 +1,19 @@
+//
+//  OklasoftCore.h
+//  OklasoftCore
+//
+//  Created by Justin Oakes on 7/16/17.
+//  Copyright © 2017 Oklasoft LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OklasoftCore.
+FOUNDATION_EXPORT double OklasoftCoreVersionNumber;
+
+//! Project version string for OklasoftCore.
+FOUNDATION_EXPORT const unsigned char OklasoftCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OklasoftCore/PublicHeader.h>
+
+
